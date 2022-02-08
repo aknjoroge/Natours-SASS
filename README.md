41fcdc78a13e472357eca27f3b6468e117db9257
Alexander karanja <alexkaranja629@gmail.com>
2022-02-08 17:08:38 +0300

    redeployed for public

---
+++ README.md
@@ -0,0 +1,18 @@
+### Natours - Advances SCSS Project
+
+### SCSS Model
+
+> 7 architecture
+
+### Components
+
+- Video background
+- grid layout
+
+### Acknowledgement
+
+#### [jonas schmedtmann](https://github.com/jonasschmedtmann)
+
+ <a href="#">
+    <img src="https://github.com/aknjoroge/" alt="Logo" width="1366" height="615">
+  </a>
---
+++ nav.js
@@ -0,0 +1,12 @@
+var list =document.getElementById('nav_list_component'); //the navigation list
+var background =document.getElementById('nav_bg_component'); //the navigation background
+var toggle =document.getElementById('navi-toggle');
+
+
+
+function nav_close() {
+//    list.style.width="0";
+//   background.style.opacity="0";
+toggle.checked =false; //unchecking the button checkbox
+
+}
